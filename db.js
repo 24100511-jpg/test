e186901d412a51b5c4fef557e3a7f6243ef8fda9
24100511-jpg <24100511@st.phenikaa-uni.edu.vn>
2026-05-31 14:38:31 +0000

    Xóa thông tin bảo mật

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: 'mysql-26d24aff-st-7a66.c.aivencloud.com',      // Nếu bạn dùng DB Cloud thì thay bằng link host Cloud nhé
     port: '21671',
     user: 'avnadmin',           
-    password: 'AVNS_tOY5dNpGX1yDAKzD0WD',           
+    password: 'YOUR_PASSWORD_HERE',           
     database: 'defaultdb', // Tên cơ sở dữ liệu của bạn
     waitForConnections: true,
     connectionLimit: 10,
